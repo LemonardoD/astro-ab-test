26a48b054dd1441c4ecd9e484a9102a5345716da
David Hordiienko <shiraz0david@gmail.com>
2026-01-13 15:45:28 +0200

    +

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,12 +1,13 @@
-export async function onRequest(context) {
-  const { request } = context;
+export async function onRequest({ request }) {
   const url = new URL(request.url);
 
   if (url.pathname === "/") {
     const group = Math.random() < 0.5 ? "a" : "b";
 
-    return fetch(new URL(`/::${group}::1`, request.url), request);
+    const file = group === "a" ? "/::a::1.html" : "/::b::1.html";
+
+    return fetch(new URL(file, request.url));
   }
 
-  return context.next();
+  return fetch(request);
 }
