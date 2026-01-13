fd26e86c1d02e84baddb314ee42fc2e8121aa32a
David Hordiienko <shiraz0david@gmail.com>
2026-01-13 15:33:14 +0200

    +

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,8 +3,7 @@ export async function onRequest(context) {
 
   if (new URL(request.url).pathname === "/") {
     const group = Math.random() < 0.5 ? "a" : "b";
-    // Rewrite by modifying the request URL
-    request.url = request.url.replace(/\/$/, `/::${group}::1`);
+    return context.rewrite(`/::${group}::1`);
   }
 
   // Proceed with the rewritten request
