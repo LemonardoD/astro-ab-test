637abf761c8d44de81da36816784e482d392e33b
David Hordiienko <shiraz0david@gmail.com>
2026-01-13 15:42:43 +0200

    +

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,12 +1,12 @@
 export async function onRequest(context) {
-  const { request, next } = context;
+  const { request } = context;
   const url = new URL(request.url);
 
   if (url.pathname === "/") {
     const group = Math.random() < 0.5 ? "a" : "b";
-    const newUrl = request.url.replace(/\/$/, `/::${group}::1`);
-    return fetch(new Request(newUrl, request));
+
+    return fetch(new URL(`/${group}`, request.url), request);
   }
 
-  return next();
+  return context.next();
 }
