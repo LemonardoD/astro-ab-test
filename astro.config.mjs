50fdae4cdff96b42ac275fb5dd305ba2fec5433f
David Hordiienko <shiraz0david@gmail.com>
2026-01-13 14:37:27 +0200

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com", // Replace with your actual site URL
+  adapter: cloudflare(),
   integrations: [
     sitemap({
       serialize: (item) => {
