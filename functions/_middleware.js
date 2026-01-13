623263ba4c84dc9308548f5460c3976434ff6c33
David Hordiienko <shiraz0david@gmail.com>
2026-01-13 15:47:56 +0200

    +

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,13 +1,11 @@
-export async function onRequest({ request }) {
+export async function onRequest({ request, next }) {
   const url = new URL(request.url);
 
   if (url.pathname === "/") {
     const group = Math.random() < 0.5 ? "a" : "b";
 
-    const file = group === "a" ? "/::a::1.html" : "/::b::1.html";
-
-    return fetch(new URL(file, request.url));
+    return fetch(new URL(`/::${group}::1`, request.url), request);
   }
 
-  return fetch(request);
+  return next();
 }
